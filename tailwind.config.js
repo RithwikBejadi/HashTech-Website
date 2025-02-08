95e7bbae7a6686a1c2dff30d5b28de9cfcb99627
Sai Rithwik Bejadi <rithwikbejadi696@gmail.com>
2025-02-08 12:31:01 +0530

    Setup and install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Ensures Tailwind scans your files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
